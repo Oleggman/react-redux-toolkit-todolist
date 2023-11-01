8d8bc18aeac4e102a141d3a456c72529257d5001
Олег <olegkovalets1112003@knu.ua>
2023-11-02 00:51:11 +0200

    Create tasks slice

---
+++ src/redux/tasksSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { addTask, deleteTask, toggleCompleted } from './actions';
+import { nanoid } from 'nanoid';
+
+const tasksInitialState = [];
+
+const tasksSlice = createSlice({
+  name: tasks,
+  initialState: tasksInitialState,
+  reducers: {
+    addTask: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(text) {
+        return {
+          payload: {
+            id: nanoid(),
+            completed: false,
+            text,
+          },
+        };
+      },
+    },
+    deleteTask(state, action) {
+      return state.filter(task => task.id != action.payload);
+    },
+    toggleCompleted(state, action) {
+      for (const task of state) {
+        if (task.id === action.payload) {
+          task.completed = !task.completed;
+          break;
+        }
+      }
+    },
+  },
+});
+
+export const { addTask, deleteTask, toggleCompleted } = tasksSlice.actions;
+export const tasksReducer = tasksSlice.reducer;
